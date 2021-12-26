5339cbeaa10c3f74edff6c9723132bf326f463ac
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2021-12-26 11:36:59 +0700

    authentication flow in app navigation

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,23 +1,50 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import AuthNavigator from "./AuthNavigator"
 import DrawerNavigator from "./DrawerNavigator"
 import { GlobalContext } from "../context/Provider";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SplashScreen from 'react-native-splash-screen'
+import { ActivityIndicator } from 'react-native'
+
 
 const AppNavContainer = () => {
 
     const {
-        authState: {isLoggedIn},
+        authState: { isLoggedIn },
     } = useContext(GlobalContext); // in the first set up, isLoggedIn = true
 
-    // console.log("isLoggedIn >> ", isLoggedIn);
-    // console.log("loading >> ", loading);
-    
+    const [isAuthenticated, setIsAuthenticated] = React.useState(isLoggedIn);
+    const [authLoaded, setAuthLoaded] = React.useState(false);
+
+    const getUser = async () => {
+        try {
+            const user = await AsyncStorage.getItem('user');
+            console.log('user islogin true ----> ', user);
+            if (user) {
+                setAuthLoaded(true);
+                setIsAuthenticated(true);
+            } else {
+                setAuthLoaded(true);
+                setIsAuthenticated(false);
+            }
+        } catch (error) { }
+    };
+
+    useEffect(() => {
+        getUser();
+    }, [isLoggedIn]);
 
     return (
-        <NavigationContainer>
-            {isLoggedIn ? <DrawerNavigator /> : <AuthNavigator />}
-        </NavigationContainer>
+        <>
+            {authLoaded ? (
+                <NavigationContainer>
+                    {isAuthenticated ? <DrawerNavigator /> : <AuthNavigator />}
+                </NavigationContainer>
+            ) : (
+                <ActivityIndicator />
+            )}
+        </>
     );
 };
 
